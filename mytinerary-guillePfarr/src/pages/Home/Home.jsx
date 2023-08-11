ec3f6a40d99f45080d71a281830bcaca83d426c7
GuillePfarr <guillepfarr@hotmail.com>
2023-08-10 22:14:52 -0400

    Home Navbar

--- mytinerary-guillePfarr/src/pages/Home/Home.jsx
+++ mytinerary-guillePfarr/src/pages/Home/Home.jsx
@@ -1,7 +1,22 @@
+import NavbarMain from "../../components/Navbar/NavbarMain"
+
+
 const Home = () => {
+  const events = [];
   return (
-    <div>Home</div>
-  )
-}
+    <div className="app-layout">
+      <header className="container app-header">
+        <NavbarMain />
+      </header>
+      <main className="app-main">
+        <section className="container d-flex flex-wrap gap-5 justify-content-between">
+        </section>
+      </main>
+      <footer className="app-footer">
+        <p className="text-center">MindHub AP MERN 088 - GuillePfarr</p>
+      </footer>
+    </div>
+  );
+};
 
 export default Home
