1d46779dbfd58934aa32fbf35f986ea7196ec322
GuillePfarr <guillepfarr@hotmail.com>
2023-08-21 07:57:54 -0400

    Creacion logo

---
+++ mytinerary-guillePfarr/public/assets/LogoMyt.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './logomyt.css'; // You can create a CSS file for styling
+
+const Logo = () => {
+    return (
+        <div className="logoMyt">MyTineraries</div>
+    );
+}
+
+export default LogoMyt;
--- mytinerary-guillePfarr/src/pages/Home/Home.jsx
+++ mytinerary-guillePfarr/src/pages/Home/Home.jsx
@@ -35,20 +35,20 @@ const Home = () => {
   return (
     <main>
       <section className="homeHero">
-          <div className="homeHeroTitle">
-         "Find your perfect trip. Designed by insiders who know and love their cities"
-       </div>
-      <img className="heroBgImage" src="https://ik.imgkit.net/3vlqs5axxjf/TW-Asia/ik-seo/uploadedImages/Industry/Travel_Agents/AdobeStock_322669629/Flight-Centre-nabs-luxury-tour-operator-Scott-Dunn.jpg?tr=w-780%2Ch-440%2Cfo-auto" alt="heroBgImage"></img>
+        <div className="homeHeroTitle">
+          "Find your perfect trip. Designed by insiders who know and love their cities"
+        </div>
+        <img className="heroBgImage" src="https://ik.imgkit.net/3vlqs5axxjf/TW-Asia/ik-seo/uploadedImages/Industry/Travel_Agents/AdobeStock_322669629/Flight-Centre-nabs-luxury-tour-operator-Scott-Dunn.jpg?tr=w-780%2Ch-440%2Cfo-auto" alt="heroBgImage"></img>
 
 
-        
-          <div className="button-wrapper">
 
-            <Link className="button cta-button" to="/Cities"></Link>
+        <div className="button-wrapper">
 
-           <p className='cta-text'>Explore Cities</p>
-          </div>
-       
+          <Link className="button cta-button" to="/Cities"></Link>
+
+          <p className='cta-text'>Explore Cities</p>
+        </div>
+
       </section>
       <section className="home-carousel">
         <Carousel />
