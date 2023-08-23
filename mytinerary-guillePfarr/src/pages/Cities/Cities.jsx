29ab6e89265384c21146a1438c9b809b58fb8145
GuillePfarr <guillepfarr@hotmail.com>
2023-08-23 14:52:07 -0400

    cities con boton

--- mytinerary-guillePfarr/src/pages/Cities/Cities.jsx
+++ mytinerary-guillePfarr/src/pages/Cities/Cities.jsx
@@ -33,7 +33,7 @@ class Cities extends Component {
   handleFilterChange(event) {
     const filterText = event.target.value.toLowerCase();
 
-    // Filtrar las ciudades que comienzan con el texto ingresado
+    
     const filteredCities = this.state.cities.filter(city =>
       city.name.toLowerCase().startsWith(filterText)
     );
@@ -88,49 +88,3 @@ export default Cities;
 
 
 
-// cities:[  [
-//             { image: "https://www.wendyperrin.com/wp-content/uploads/2018/08/Cape-Town-Aerial-view-1-Photo-NAC-Helicopters.jpg", name: "Cape Town-South Africa" },
-//       { image: "https://www.wendyperrin.com/wp-content/uploads/2018/07/Teotihuacan-pyramid-balloons-near-Mexico-City-shutterstock_718460332.jpg", name: "Mexico-City Mexico" },
-//       { image: "https://www.wendyperrin.com/wp-content/uploads/2018/10/Galata-Tower-neighberhood-view-Istanbul-Turkey-shutterstock_554343394.jpg", name: "Istambul-Turkey" },
-//       { image: "https://www.wendyperrin.com/wp-content/uploads/2017/12/shutterstock_388289746.jpg", name: "Barcelona-Spain" },
-//       { image: "https://tnaa.com/wp-content/uploads/2021/10/hero-684x620.jpg", name: "Ciudad 4" }
-//     ],
-//     [
-//       { image: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/Calle_Dlugie_Pobrzeze%2C_Gdansk%2C_Polonia%2C_2013-05-20%2C_DD_06.jpg/1024px-Calle_Dlugie_Pobrzeze%2C_Gdansk%2C_Polonia%2C_2013-05-20%2C_DD_06.jpg", name: "Ciudad 5" },
-//       { image: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Hagia_Sophia_Mars_2013.jpg/800px-Hagia_Sophia_Mars_2013.jpg", name: "Istambul" },
-//       { image: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Luzern_Kapellbruecke.jpg/800px-Luzern_Kapellbruecke.jpg", name: "Lucerne Swizerland" },
-//       { image: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/Kremlevskaya_Naberezhnaja_Moscow.hires.jpg/800px-Kremlevskaya_Naberezhnaja_Moscow.hires.jpg", name: "Moscow Russia" },
-//     ],
-// [
-//       { image: "https://www.wendyperrin.com/wp-content/uploads/2019/08/Gran-Via-night-Madrid-Spain-shutterstock_557009335.jpg", name: "Madrid-Spain" },
-//       { image: "https://www.wendyperrin.com/wp-content/uploads/2018/06/stock-photo-mountain-village-novara-di-sicilia-sicily-italy-shutterstock_736870174.jpg", name: "Sicily-Italy" },
-//       { image: "https://www.wendyperrin.com/wp-content/uploads/2018/01/venice-2647436_1920.jpg", name: "Venice-Italy" },
-//       { image: "https://www.wendyperrin.com/wp-content/uploads/2014/09/duomo-florence-italy-cr-brian-dore-1cropped.jpg", name: "Florence-Italy" },
-//     ],
-// [
-//       { image: "https://www.wendyperrin.com/wp-content/uploads/2019/08/Gran-Via-night-Madrid-Spain-shutterstock_557009335.jpg", name: "Madrid-Spain" },
-//       { image: "https://www.wendyperrin.com/wp-content/uploads/2018/06/stock-photo-mountain-village-novara-di-sicilia-sicily-italy-shutterstock_736870174.jpg", name: "Sicily-Italy" },
-//       { image: "https://w0.peakpx.com/wallpaper/966/513/HD-wallpaper-compass-to-navigate-directions-navigation-compass-map-firefox-persona-theme-vintage.jpg", name: "Hero-one" },
-//       { image: "https://ik.imgkit.net/3vlqs5axxjf/TW-Asia/ik-seo/uploadedImages/Industry/Travel_Agents/AdobeStock_322669629/Flight-Centre-nabs-luxury-tour-operator-Scott-Dunn.jpg?tr=w-780%2Ch-440%2Cfo-auto", name: "Hero-two" },
-//     ],
-//     [
-//       { image: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b4/London_Eye_Twilight_April_2006.jpg/800px-London_Eye_Twilight_April_2006.jpg", name: "London England" },
-//       { image:"https://upload.wikimedia.org/wikipedia/commons/thumb/7/7f/HerdenkingVuurgrensRotterdam1940_2007_edit1.jpg/800px-HerdenkingVuurgrensRotterdam1940_2007_edit1.jpg", name: "Rotterdam Netherlands" },
-//       { image: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d8/Colosseum_in_Rome-April_2007-1-_copie_2B.jpg/1024px-Colosseum_in_Rome-April_2007-1-_copie_2B.jpg", name: "Rome Italy" },
-//       { image: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b0/Attica_06-13_Athens_25_Olympian_Zeus_Temple.jpg/800px-Attica_06-13_Athens_25_Olympian_Zeus_Temple.jpg", name: "Athens Greek" },
-//     ],]
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-   
