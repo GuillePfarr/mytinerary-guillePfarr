02fa7704e85a34761ac559ea2787d0d31499ea6f
GuillePfarr <guillepfarr@hotmail.com>
2023-09-09 17:38:56 -0300

    Footer ancho total-itineraries cards mejorando-

--- mytinerary-guillePfarr/src/components/Details/CityDetails.jsx
+++ mytinerary-guillePfarr/src/components/Details/CityDetails.jsx
@@ -1,3 +1,71 @@
+// import React, { useEffect } from 'react';
+// import { Link, useParams } from 'react-router-dom';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { fetchCityDetails, fetchCityItineraries } from '../../redux/reducers/cityDetailsSlice'; 
+
+// import '../Details/citydetails.css';
+
+// const CityDetails = () => {
+//   const { id } = useParams();
+//   const dispatch = useDispatch();
+
+  
+//   const city = useSelector((state) => state.cityDetails.city);
+//   const itineraries = useSelector((state) => state.cityDetails.itineraries);
+
+//   useEffect(() => {
+    
+//     dispatch(fetchCityDetails(id));
+//     dispatch(fetchCityItineraries(id));
+//   }, [dispatch, id]);
+
+//   const cardStyle = {
+//     width: '60vw',
+//     height: '40vh',
+//   };
+
+//   return (
+//     <div className="container">
+//       <div className="underConstContainer"></div>
+//       <h2>City Details</h2>
+//       <div className="card" style={cardStyle}>
+//         <div className="card-body">
+//           <img src={city.image} className="card-img-top" alt={city.name} />
+//           <h5 className="card-title">{city.name}</h5>
+//         </div>
+//       </div>
+
+//       <h3>Itineraries for {city.name}</h3>
+//       <div className="itineraries">
+//         {itineraries.length > 0 ? (
+//           itineraries.map((itinerary) => (
+//             <div key={itinerary._id} className="card" style={cardStyle}>
+//               <img src={itinerary.user_image} className="details-card-img-top" alt={itinerary.user_name} />
+//               <div className="card-body">
+//                 <h5 className="card-title">{itinerary.user_name}</h5>
+//                 <p className="card-text">Price: {itinerary.price}</p>
+//                 <p className="card-text">Duration: {itinerary.duration} hours</p>
+//               </div>
+//             </div>
+//           ))
+//         ) : (
+//           <div className="no-itineraries-card" style={cardStyle}>
+//             <div className="under-const-card-body">
+//               <p className="under-const-card-text"><strong>This city does not have itineraries yet.</strong></p>
+//             </div>
+//           </div>
+//         )}
+//       </div>
+
+//       <Link to="/cities" className="btn btn-primary">
+//         Back to Cities
+//       </Link>
+//     </div>
+//   );
+// };
+
+// export default CityDetails;
+
 import React, { useEffect } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
@@ -8,13 +76,13 @@ import '../Details/citydetails.css';
 const CityDetails = () => {
   const { id } = useParams();
   const dispatch = useDispatch();
-
   
+  // Obtén los datos de Redux store
   const city = useSelector((state) => state.cityDetails.city);
   const itineraries = useSelector((state) => state.cityDetails.itineraries);
 
   useEffect(() => {
-    
+    // Dispara las acciones para cargar datos
     dispatch(fetchCityDetails(id));
     dispatch(fetchCityItineraries(id));
   }, [dispatch, id]);
@@ -36,15 +104,23 @@ const CityDetails = () => {
       </div>
 
       <h3>Itineraries for {city.name}</h3>
-      <div className="itineraries">
+      
         {itineraries.length > 0 ? (
           itineraries.map((itinerary) => (
-            <div key={itinerary._id} className="card" style={cardStyle}>
-              <img src={itinerary.user_image} className="details-card-img-top" alt={itinerary.user_name} />
-              <div className="card-body">
-                <h5 className="card-title">{itinerary.user_name}</h5>
-                <p className="card-text">Price: {itinerary.price}</p>
-                <p className="card-text">Duration: {itinerary.duration} hours</p>
+            <div key={itinerary._id} className="itinerary-card" style={cardStyle}>
+              <div className="itineraries-card-body">
+                <img src={itinerary.user_image} className="details-card-img-top" alt={itinerary.user_name} />
+                <h5 className="itinerary-user-name">{itinerary.user_name}</h5>
+                <p className="itinerary-price">Price: {itinerary.price}</p>
+                <p className="itinerary-duration">Duration: {itinerary.duration} hours</p>
+                <div className='itinerary-hashtags'>
+                  <p>Hashtags:</p>
+                  <ul>
+                    {itinerary.hashtag.map((tag, index) => (
+                      <li key={index}>{tag}</li>
+                    ))}
+                  </ul>
+                </div>
               </div>
             </div>
           ))
@@ -55,7 +131,7 @@ const CityDetails = () => {
             </div>
           </div>
         )}
-      </div>
+      
 
       <Link to="/cities" className="btn btn-primary">
         Back to Cities
@@ -65,4 +141,3 @@ const CityDetails = () => {
 };
 
 export default CityDetails;
-
