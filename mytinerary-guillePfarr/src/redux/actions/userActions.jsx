bf1b198874900bd502b32c135eacd9bb201d6977
GuillePfarr <guillepfarr@hotmail.com>
2023-09-14 07:42:16 -0300

    usuario creado desde front

--- mytinerary-guillePfarr/src/redux/actions/userActions.jsx
+++ mytinerary-guillePfarr/src/redux/actions/userActions.jsx
@@ -9,7 +9,7 @@ export const cargarUsuario = createAction('cargar_usuario', (user) => {
 
 export const signUp = createAsyncThunk("create_user", async (body) => {
     try {
-        const response = await axios.post("http://localhost:3000/api/auth/signUp", body)
+        const response = await axios.post("http://localhost:3000/api/auth/signup", body)
         localStorage.setItem('token', response.data.token)
         console.log(response)
         return response.data.user
@@ -17,6 +17,38 @@ export const signUp = createAsyncThunk("create_user", async (body) => {
     } catch (error) {
         console.log(error)
     }
-
-
 })
+
+export const signIn = createAsyncThunk("logear", async (body) => {
+    try {
+        const response = await axios.post("http://localhost:3000/api/auth/signin", body)
+        localStorage.setItem('token', response.data.token)
+        console.log(response)
+        return response.data
+
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+
+export const signInWithToken = createAsyncThunk("logear_token", async (   ) => {
+    try {
+
+        const token = localStorage.getItem(' token')
+        const response = await axios.post("http://localhost:3000/api/auth/signin/token",{}, {
+               headers:{
+           Authorization: " Bearer" + token
+}
+})             
+        console.log(response)
+        return response.data.user
+
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+
+
+
