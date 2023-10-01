5ef9b067193b68ebf9368f9c0d60732e2ff923b2
GuillePfarr <guillepfarr@hotmail.com>
2023-10-01 11:41:46 -0300

    Navbar menu

--- mytinerary-guillePfarr/src/components/NavbarMain/NavbarMain.jsx
+++ mytinerary-guillePfarr/src/components/NavbarMain/NavbarMain.jsx
@@ -21,8 +21,9 @@ const NavbarMain = () => {
         <button
           className={`ToggleButton ${showMenu ? 'CloseButton' : ''}`}
           onClick={() => setShowMenu(!showMenu)}
-        >
-          {showMenu ? 'Close' : 'Menu'}
+        >   
+        {showMenu ? 'X' : '☰'}
+          
         </button>
       </div>
       <nav className={`NavbarLinks ${showMenu ? 'ShowMenu' : ''}`}>
@@ -63,69 +64,5 @@ const NavbarMain = () => {
 
 export default NavbarMain;
 
-// import React, { useState } from 'react';
-// import { Link } from 'react-router-dom';
-// import './NavbarMain.css';
-// import { useSelector, useDispatch } from 'react-redux';
-// import { logout } from '../../redux/actions/userActions';
 
-// const NavbarMain = () => {
-//   const [showMenu, setShowMenu] = useState(false);
-//   const { token } = useSelector((store) => store.citiesReducer);
-//   const user = useSelector((store) => store.userReducer.user);
-//   const dispatch = useDispatch();
-
-//   const toggleMenu = () => {
-//     setShowMenu(!showMenu);
-//   };
-
-//   return (
-//     <header className="HeaderMain">
-//       <div className="NavbarContainer">
-//         <img className="LogoMyTinerary" src="../public/MyTinerary.png" alt="MyTinerary Logo" />
-//         <h1 className="MyTNabvar">MyTinerary</h1>
-
-//         <button
-//           className={`ToggleButton ${showMenu ? 'CloseButton' : ''}`}
-//           onClick={toggleMenu}
-//         >
-//           ☰ {/* Icono de hamburguesa */}
-//         </button>
-//       </div>
-//       <nav className={`NavbarLinks ${showMenu ? 'ShowMenu' : ''}`}>
-//         <ul>
-//           <li>
-//             <Link to="/">Home</Link>
-//           </li>
-//           <li>
-//             <Link to="/cities">Cities</Link>
-//           </li>
-//           {user ? (
-//             <li className="user-profile">
-//               <img
-//                 className="userImage"
-//                 src={user.image || "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvIFhtkP1KVLsieF9iXxl1HN63NJSdNcbzFOkLztujFA&s"}
-//                 alt="User"
-//               />
-//               <button className="buttonLogout" onClick={() => dispatch(logout())}>
-//                 Log Out
-//               </button>
-//             </li>
-//           ) : (
-//             <>
-//               <li>
-//                 <Link to="/signup">Sign Up</Link>
-//               </li>
-//               <li>
-//                 <Link to="/signin">Sign In</Link>
-//               </li>
-//             </>
-//           )}
-//         </ul>
-//       </nav>
-//     </header>
-//   );
-// };
-
-// export default NavbarMain;
 
