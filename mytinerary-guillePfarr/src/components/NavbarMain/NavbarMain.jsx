233b9a1a0b2a626d530b2083c048349f0120e34c
GuillePfarr <guillepfarr@hotmail.com>
2023-10-01 11:10:53 -0300

    Navbar

--- mytinerary-guillePfarr/src/components/NavbarMain/NavbarMain.jsx
+++ mytinerary-guillePfarr/src/components/NavbarMain/NavbarMain.jsx
@@ -1,59 +1,3 @@
-// import React, { useState } from 'react';
-// import { Link } from 'react-router-dom';
-// import './NavbarMain.css';
-// import { useSelector, useDispatch } from 'react-redux';
-// import { logout } from '../../redux/actions/userActions';
-
-// const NavbarMain = () => {
-//   const [showMenu, setShowMenu] = useState(false);
-//   const { token } = useSelector(store => store.citiesReducer)
-//   const user = useSelector((store) => store.userReducer.user)
-//   const dispatch = useDispatch()
-//   return (
-//     <header className="HeaderMain">
-//       <div className="NavbarContainer">
-//         <img className='LogoMyTinerary' src="../public/MyTinerary.png"></img>
-//         <h1 className='MyTNabvar'>      MyTinerary     </h1>
-
-//         <button
-//           className={`ToggleButton ${showMenu ? 'CloseButton' : ''}`}
-//           onClick={() => setShowMenu(!showMenu)}>
-
-//           {showMenu ? 'Close' : 'Menu'}
-//         </button>
-//       </div>
-//       <nav className={`NavbarLinks ${showMenu ? 'ShowMenu' : ''}`}>
-//         <ul>
-//           <li>
-//             <Link to="/">Home</Link>
-//           </li>
-//           <li>
-//             <Link to="/cities">Cities</Link>
-//           </li>
-//           {user ? (
-//             <li>
-//               <button className='buttonLogout' onClick={() => dispatch(logout())}>LogOut</button>
-//             </li>
-//           ) : (
-//             <>
-//               <li>
-//                 <Link to="/signup">SignUp</Link>
-//               </li>
-//               <li>
-//                 <Link to="/signin">SignIn</Link>
-//               </li>
-//             </>
-//           )}
-//         </ul>
-
-//       </nav>
-//     </header>
-//   );
-// };
-
-// export default NavbarMain;
-
-
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './NavbarMain.css';
@@ -70,9 +14,10 @@ const NavbarMain = () => {
   return (
     <header className="HeaderMain">
       <div className="NavbarContainer">
+      <div className="LogoAndTitle">
         <img className="LogoMyTinerary" src="../public/MyTinerary.png" alt="MyTinerary Logo" />
         <h1 className="MyTNabvar">MyTinerary</h1>
-
+      </div>
         <button
           className={`ToggleButton ${showMenu ? 'CloseButton' : ''}`}
           onClick={() => setShowMenu(!showMenu)}
