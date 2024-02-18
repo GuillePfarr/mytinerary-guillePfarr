cd31c88f9eb0456a695c7ac96be202d530378fd3
GuillePfarr <guillepfarr@hotmail.com>
2024-02-18 08:33:07 -0300

    Modified cards vanguard

--- src/pages/Vanguard/Vanguard.jsx
+++ src/pages/Vanguard/Vanguard.jsx
@@ -3,37 +3,37 @@ import { Link, useLocation } from 'react-router-dom';
 import axios from 'axios';
 
 function Vanguard() {
-  const [sensors, setSensors] = useState([]);
+  const [vanguards, setVanguards] = useState([]);
   const [filter, setFilter] = useState('');
-  const [filteredSensors, setFilteredSensors] = useState([]);
+  const [filteredVanguards, setFilteredVanguards] = useState([]);
   const [noResults, setNoResults] = useState(false);
 
-  const handleFilterChange = (event) => {
-    const filterText = event.target.value.toLowerCase();
+  const handleFilterChange = (vanguard) => {
+    const filterText = vanguard.target.value.toLowerCase();
 
-    const filteredSensors = sensors.filter((sensor) =>
-      sensor.tempInt1.toString().startsWith(filterText) ||
-      sensor.tempInt2.toString().startsWith(filterText)
+    const filteredSensors = vanguards.filter((sensor) =>
+      vanguard.tempInt1.toString().startsWith(filterText) ||
+      vanguard.tempInt2.toString().startsWith(filterText)
     );
 
-    setFilter(event.target.value);
-    setFilteredSensors(filteredSensors);
+    setFilter(vanguard.target.value);
+    setFilteredVanguards(filteredVanguards);
   };
 
   useEffect(() => {
     axios
       .get(import.meta.env.VITE_API_URL + '/api/vanguards')
       .then((res) => {
-        setSensors(res.data.response);
-        setFilteredSensors(res.data.response);
+        setVanguards(res.data.response);
+        setFilteredVanguards(res.data.response);
       })
       .catch((error) => console.log(error));
   }, []);
 
   useEffect(() => {
-    setNoResults(filteredSensors.length === 0);
+    setNoResults(filteredVanguards.length === 0);
     window.scrollTo(0, 0);
-  }, [filteredSensors]);
+  }, [filteredVanguards]);
 
   return (
     <div className="sensors-container">
@@ -49,12 +49,12 @@ function Vanguard() {
         <h2 className="display-1 fw-bolder">No matches.</h2>
       )}
       <div className="cardsfield">
-        {filteredSensors.map((sensor, index) => (
+        {filteredVanguards.map((vanguard, index) => (
           <div className="card" key={index}>
             <div className="card-body">
               <h5 className="card-title">Temperature Sensor {index + 1}</h5>
-              <p>Temperature 1: {sensor.tempInt1} °C</p>
-              <p>Temperature 2: {sensor.tempInt2} °C</p>
+              <p>Temperature 1: {vanguard.tempInt1} °C</p>
+              <p>Temperature 2: {vanguard.tempInt2} °C</p>
             </div>
           </div>
         ))}
