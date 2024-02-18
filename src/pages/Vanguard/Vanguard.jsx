24781f8652102ecd1b725b21b8c37584de6517ee
GuillePfarr <guillepfarr@hotmail.com>
2024-02-18 06:37:15 -0300

    Modified Vanguard

---
+++ src/pages/Vanguard/Vanguard.jsx
@@ -0,0 +1,65 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function Sensors() {
+  const [sensors, setSensors] = useState([]);
+  const [filter, setFilter] = useState('');
+  const [filteredSensors, setFilteredSensors] = useState([]);
+  const [noResults, setNoResults] = useState(false);
+
+  const handleFilterChange = (event) => {
+    const filterText = event.target.value.toLowerCase();
+
+    const filteredSensors = sensors.filter((sensor) =>
+      sensor.tempInt1.toString().startsWith(filterText) ||
+      sensor.tempInt2.toString().startsWith(filterText)
+    );
+
+    setFilter(event.target.value);
+    setFilteredSensors(filteredSensors);
+  };
+
+  useEffect(() => {
+    axios
+      .get(import.meta.env.VITE_API_URL + '/api/vanguards')
+      .then((res) => {
+        setSensors(res.data.response);
+        setFilteredSensors(res.data.response);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
+  useEffect(() => {
+    setNoResults(filteredSensors.length === 0);
+    window.scrollTo(0, 0);
+  }, [filteredSensors]);
+
+  return (
+    <div className="sensors-container">
+      <h1 className='SensorsTitle'>Temperature Sensors</h1>
+      <input
+        className='inputSensors'
+        type="text"
+        placeholder="Filter by temperature..."
+        value={filter}
+        onChange={handleFilterChange}
+      />
+      {noResults && (
+        <h2 className="display-1 fw-bolder">No matches.</h2>
+      )}
+      <div className="cardsfield">
+        {filteredSensors.map((sensor, index) => (
+          <div className="card" key={index}>
+            <div className="card-body">
+              <h5 className="card-title">Temperature Sensor {index + 1}</h5>
+              <p>Temperature 1: {sensor.tempInt1} °C</p>
+              <p>Temperature 2: {sensor.tempInt2} °C</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Sensors;
