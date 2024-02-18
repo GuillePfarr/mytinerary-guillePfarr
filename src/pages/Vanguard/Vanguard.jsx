a3f7219999b582edecbdb3c9e55240ffdab852fc
GuillePfarr <guillepfarr@hotmail.com>
2024-02-18 07:28:05 -0300

    Modify Vanguardjsx

--- src/pages/Vanguard/Vanguard.jsx
+++ src/pages/Vanguard/Vanguard.jsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import axios from 'axios';
 
-function Sensors() {
+function Vanguard() {
   const [sensors, setSensors] = useState([]);
   const [filter, setFilter] = useState('');
   const [filteredSensors, setFilteredSensors] = useState([]);
