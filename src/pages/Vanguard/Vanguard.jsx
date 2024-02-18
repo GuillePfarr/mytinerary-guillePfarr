6102af06cdd5b4453fa9bb0c3cf147c00bf1ea76
GuillePfarr <guillepfarr@hotmail.com>
2024-02-18 09:51:21 -0300

    Polling ok

--- src/pages/Vanguard/Vanguard.jsx
+++ src/pages/Vanguard/Vanguard.jsx
@@ -1,61 +1,3 @@
-// import React, { useState, useEffect } from 'react';
-// import axios from 'axios';
-
-// function Vanguard() {
-//   const [vanguards, setVanguards] = useState([]);
-//   const [noResults, setNoResults] = useState(false);
-
-//   useEffect(() => {
-//     axios
-//       .get(import.meta.env.VITE_API_URL + '/api/vanguard')
-//       .then((res) => {
-//         setVanguards(res.data.response);
-//       })
-//       .catch((error) => console.log(error));
-//   }, []);
-
-
-
-
-//  useEffect(() => {
-//     fetchData(); 
-
-//     const intervalId = setInterval(() => {
-//       fetchData(); 
-//     }, 30000);
-
-//     return () => clearInterval(intervalId); 
-
-//   useEffect(() => {
-//     setNoResults(vanguards.length === 0);
-//     window.scrollTo(0, 0);
-//   }, [vanguards]);
-
-//   return (
-//     <div className="sensors-container">
-//       <h1 className='SensorsTitle'>Temperature Sensors</h1>
-//       {noResults && (
-//         <h2 className="display-1 fw-bolder">No matches.</h2>
-//       )}
-//       <div className="cardsfield">
-//         {vanguards.map((vanguard) => (
-//           <div className="card" key={vanguard._id}>
-//             <div className="card-body">
-//               <h5 className="card-title">Temperature Sensor {vanguard._id}</h5>
-//               <p>Temperature 1: {vanguard.tempInt1} °C</p>
-//               {vanguard.tempInt2 && <p>Temperature 2: {vanguard.tempInt2} °C</p>}
-//             </div>
-//           </div>
-//         ))}
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default Vanguard;
-
-
-
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
@@ -73,14 +15,14 @@ function Vanguard() {
   };
 
   useEffect(() => {
-    fetchData(); // Llamada inicial al montar el componente
+    fetchData(); 
 
     const intervalId = setInterval(() => {
-      fetchData(); // Llamada adicional cada 30 segundos
+      fetchData(); 
     }, 30000);
 
-    return () => clearInterval(intervalId); // Limpiar el intervalo al desmontar el componente
-  }, []); // <-- ¡Asegúrate de pasar un array vacío aquí!
+    return () => clearInterval(intervalId); 
+  }, []); 
 
   useEffect(() => {
     setNoResults(vanguards.length === 0);
