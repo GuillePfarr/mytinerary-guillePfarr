d8115b41349ab5c936477a22c1a7a0455474b067
GuillePfarr <guillepfarr@hotmail.com>
2024-02-25 10:18:53 -0300

    Log Raw date

--- src/pages/Vanguard/Vanguard.jsx
+++ src/pages/Vanguard/Vanguard.jsx
@@ -239,6 +239,7 @@ import axios from 'axios';
 
 
 const formatDateTime = (dateTimeString) => {
+    console.log("Raw date:", dateTimeString);
     if (!dateTimeString) return "No data available";
     console.log("Invalid date:", dateTimeString);
     const options = { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit', timeZoneName: 'short' };
