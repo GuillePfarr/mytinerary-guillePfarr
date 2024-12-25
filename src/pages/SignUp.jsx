262d497fceb61b4ffb89577c7af57f20230ee4d7
GuillePfarr <guillepfarr@hotmail.com>
2024-12-25 13:47:57 -0300

    useState instead of useRef in SignUp

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,59 +1,54 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import './SignUp.css';
 import { useDispatch } from 'react-redux';
-import { Link, useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-import { signUp } from '../redux/actions/userActions'
+import { signUp } from '../redux/actions/userActions';
 import { GoogleLogin, GoogleOAuthProvider } from '@react-oauth/google';
 import jwtDecode from 'jwt-decode';
 
-const setData = ({
-    email: "",
-    name: "",
-    password: "",
-    terms: false
-
-})
-
 const SignUp = () => {
     const [countries, setCountries] = useState([]);
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        password: "",
+        image: "",
+        country: ""
+    });
 
-    const dispatch = useDispatch()
+    const dispatch = useDispatch();
 
-    const name = useRef(null)
-    const email = useRef(null)
-    const password = useRef(null)
-    const image = useRef(null)
-    const country = useRef(null)
-
-
+    // Fetch countries on mount
     useEffect(() => {
         axios("https://restcountries.com/v3.1/all?fields=name").then(({ data }) =>
             setCountries(data.map((country) => country.name.common))
         );
     }, []);
 
+    // Handle input changes
+    const handleInputChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        });
+    };
+
+    // Handle form submission
     const handleSubmit = (e) => {
         e.preventDefault();
-        const aux = [name, email, password, image, country];
-        if (aux.some((campo) => !campo.current.value)) {
+        const { name, email, password, image, country } = formData;
+
+        if (!name || !email || !password || !image || !country) {
             alert("All fields are required");
         } else {
-            const body = {
-                name: name.current.value,
-                email: email.current.value,
-                password: password.current.value,
-                image: image.current.value,
-                country: country.current.value,
-            };
-            
-            dispatch(signUp(body))
+            dispatch(signUp(formData));
         }
     };
 
+    // Handle Google login
     const handleSubmitGoogle = async (data) => {
-       
         const body = {
             name: data.given_name + " " + data.family_name,
             email: data.email,
@@ -61,56 +56,84 @@ const SignUp = () => {
             image: data.picture,
             country: "Argentina",
         };
-        
-        dispatch(signUp(body))
-    }
+
+        dispatch(signUp(body));
+    };
+
     return (
         <div className="signup-container">
             <form className="signup-form" onSubmit={handleSubmit}>
                 <label className="signup-label">
                     Name
-                    <input type="text" name="name" className="signup-input" ref={name} />
+                    <input
+                        type="text"
+                        name="name"
+                        className="signup-input"
+                        value={formData.name}
+                        onChange={handleInputChange}
+                    />
                 </label>
                 <label className="signup-label">
                     Email
-                    <input type="text" name="email" className="signup-input" ref={email} />
+                    <input
+                        type="email"
+                        name="email"
+                        className="signup-input"
+                        value={formData.email}
+                        onChange={handleInputChange}
+                    />
                 </label>
                 <label className="signup-label">
                     Password
-                    <input type="text" name="password" className="signup-input" ref={password} />
+                    <input
+                        type="password"
+                        name="password"
+                        className="signup-input"
+                        value={formData.password}
+                        onChange={handleInputChange}
+                    />
                 </label>
                 <label className="signup-label">
                     Image
-                    <input type="text" name="image" className="signup-input" ref={image} />
+                    <input
+                        type="text"
+                        name="image"
+                        className="signup-input"
+                        value={formData.image}
+                        onChange={handleInputChange}
+                    />
                 </label>
                 <label className="signup-label">
                     Country
-                    <select name="country" className="signup-input" ref={country}>
-                        {countries.length > 0 &&
-                            countries.map((country, index) => (
-                                <option key={index} value={country}>
-                                    {country}
-                                </option>
-                            ))}
+                    <select
+                        name="country"
+                        className="signup-input"
+                        value={formData.country}
+                        onChange={handleInputChange}
+                    >
+                        <option value="">Select a country</option>
+                        {countries.map((country, index) => (
+                            <option key={index} value={country}>
+                                {country}
+                            </option>
+                        ))}
                     </select>
                 </label>
-                <button className='bt btn-secondary' type="submit">Registrarse</button>
+                <button className="bt btn-secondary" type="submit">Registrarse</button>
                 <GoogleOAuthProvider clientId="445761792247-dbcpi8hmi2o5mv47rjaam9l30eqq4uku.apps.googleusercontent.com">
                     <GoogleLogin
                         onSuccess={credentialResponse => {
-                            
-                            const infoUser = jwtDecode(credentialResponse.credential)
-                            handleSubmitGoogle(infoUser)
+                            const infoUser = jwtDecode(credentialResponse.credential);
+                            handleSubmitGoogle(infoUser);
                         }}
                         onError={() => {
-                            console.log('Login Failed')
+                            console.log('Login Failed');
                         }}
-                    />;
-                </GoogleOAuthProvider>;
+                    />
+                </GoogleOAuthProvider>
                 <div className="button-wrapper">
-                    <Link className="button cta-signup-button" to="/signin"></Link>
-                    <p className='cta-text'>Already registered?</p>
-
+                    <Link className="button cta-signup-button" to="/signin">Sign In</Link>
+                    <p className="cta-text">Already registered?</p>
                 </div>
             </form>
         </div>
@@ -118,3 +141,4 @@ const SignUp = () => {
 };
 
 export default SignUp;
+
