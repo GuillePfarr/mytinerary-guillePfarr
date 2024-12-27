ba45de7a21ea7011485e93a9ccbbcf2d03107071
GuillePfarr <guillepfarr@hotmail.com>
2024-12-27 10:02:52 -0300

    New version Vanguard.jsx

--- src/pages/Vanguard/Vanguard.jsx
+++ src/pages/Vanguard/Vanguard.jsx
@@ -1,3 +1,87 @@
+// import React, { useState, useEffect } from 'react';
+// import axios from 'axios';
+// import "./vanguard.css";
+
+// const formatTime = (dateTimeString) => {
+//   if (!dateTimeString) return "No data available";
+
+//   const options = { hour: '2-digit', minute: '2-digit' };
+//   const localDate = new Date(dateTimeString);
+
+//   return localDate.toLocaleString('en-US', options);
+// };
+
+// function Vanguard() {
+//   const [vanguardData, setVanguardData] = useState(null);
+//   const [, setParameters] = useState({
+//     tempMin: null,
+//     tempMax: null,
+//     humidityMin: null,
+//     humidityMax: null
+//   });
+
+
+
+// useEffect(() => {
+//   const fetchData = async () => {
+//     try {
+     
+//       const vanguardResponse = await axios.get(import.meta.env.VITE_API_URL + '/api/vanguard');
+//       const vanguards = vanguardResponse.data.response;
+//       console.log('Datos de Parameters:', vanguardData);
+//       const currentTemperatureObj = vanguards.find((vanguard) => vanguard._id === '6609c0e76cfbe770c4735e09');
+//       setVanguardData(currentTemperatureObj);
+
+    
+     
+//     } catch (error) {
+//       console.error('Error fetching data:', error);
+//     }
+//   };
+
+//   fetchData();
+// }, []);
+
+
+//   return (
+//     <div className="vanguard-container">
+//       <h1 className="VanguardTitle">Vanguard Data</h1>
+
+//       {vanguardData && (
+//         <div className="vanguard-content">
+         
+//           <div className="card data-card">
+//             <div className="card-body">
+//               <h5 className="card-title">Current Data</h5>
+//               <p>Current Temperature: {vanguardData.tempInt1} °C</p>
+//               <p>Max Temperature: {vanguardData.tempInt1Max} °C</p>
+//               <p>Min Temperature: {vanguardData.tempInt1Min} °C</p>
+//               <p>Estado de Error: {vanguardData.errorStatus}</p>
+//               <p>Time: {formatTime(vanguardData.date)}</p>
+//             </div>
+//           </div>
+
+        
+//           <div className="card parameters-card">
+//             <div className="card-body">
+//               <h5 className="card-title">Parameters</h5>
+//               <div>
+//                 <p>Min Temperature: {parameters.tempMin} °C</p>
+//                 <p>Max Temperature: {parameters.tempMax} °C</p>
+//                 <p>Min Humidity: {parameters.humidityMin} %</p>
+//                 <p>Max Humidity: {parameters.humidityMax} %</p>
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//       )}
+//     </div>
+//   );
+// }
+
+// export default Vanguard;
+
+
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import "./vanguard.css";
@@ -13,129 +97,27 @@ const formatTime = (dateTimeString) => {
 
 function Vanguard() {
   const [vanguardData, setVanguardData] = useState(null);
-  const [parameters, setParameters] = useState({
-    tempMin: null,
-    tempMax: null,
-    humidityMin: null,
-    humidityMax: null
-  });
+  const [ajustesData, setAjustesData] = useState(null);
 
-  
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     try {
-        
-  //       const vanguardResponse = await axios.get(import.meta.env.VITE_API_URL + '/api/vanguard');
-  //       const vanguards = vanguardResponse.data.response;
-  //       const currentTemperatureObj = vanguards.find((vanguard) => vanguard._id === '6609c0e76cfbe770c4735e09');
-  //       setVanguardData(currentTemperatureObj);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        // Fetch vanguard data
+        const vanguardResponse = await axios.get(import.meta.env.VITE_API_URL + '/api/vanguard');
+        const vanguards = vanguardResponse.data.response;
+        const currentTemperatureObj = vanguards.find((vanguard) => vanguard._id === '6609c0e76cfbe770c4735e09');
+        setVanguardData(currentTemperatureObj);
 
-  //       const parameterResponse = await axios.get(import.meta.env.VITE_API_URL + '/api/parameter');
-  //       const parameters = parameterResponse.data.response;
-  //       const parametersObj = parameters.find((parameter) => parameter._id === '676d6f02d6ae0a6ca083bee6'); 
-  //       setParameterData(parametersObj)
-  //     } catch (error) {
-  //       console.error('Error fetching data:', error);
-  //     }
-  //   };
+        // Fetch ajustes data
+        const ajustesResponse = await axios.get('https://mytinerary-2s20.onrender.com/api/ajuste/676e68bcce18f424bfd35d5a');
+        setAjustesData(ajustesResponse.data); // Guardamos los datos de ajustes
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
 
-  //   fetchData();
-  //   const interval = setInterval(fetchData, 10000); 
-
-  //   return () => clearInterval(interval);
-  // }, []);
-
-
-// useEffect(() => {
-//   const fetchData = async () => {
-//     try {
-      
-//       const vanguardResponse = await axios.get(import.meta.env.VITE_API_URL + '/api/vanguard');
-//       const vanguards = vanguardResponse.data.response;
-//       const currentTemperatureObj = vanguards.find((vanguard) => vanguard._id === '6609c0e76cfbe770c4735e09');
-//       setVanguardData(currentTemperatureObj);
-
-    
-//       const parameterId = '676d6f02d6ae0a6ca083bee6'; 
-//       const parameterResponse = await axios.get(import.meta.env.VITE_API_URL + `/api/parameter/${parameterId}`);
-//       const parameterObj = parameterResponse.data.response;
-
-      
-//       if (parameterObj) {
-//         setParameterData(parameterObj);
-//       } else {
-//         console.error('Parameter with the given ID not found.');
-//       }
-//     } catch (error) {
-//       console.error('Error fetching data:', error);
-//     }
-//   };
-
-
-// useEffect(() => {
-//   const fetchData = async () => {
-//     try {
-      // Fetch vanguard data
-      // const vanguardResponse = await axios.get(import.meta.env.VITE_API_URL + '/api/vanguard');
-      // const vanguards = vanguardResponse.data.response;
-      // const currentTemperatureObj = vanguards.find((vanguard) => vanguard._id === '6609c0e76cfbe770c4735e09');
-      // setVanguardData(currentTemperatureObj);
-
-      // Fetch a specific parameter using the ID in the URL
-      // const parameterId = '676d6f02d6ae0a6ca083bee6'; 
-      // const parameterResponse = await axios.get(import.meta.env.VITE_API_URL + `/api/parameter/${parameterId}`);
-      // const parameterObj = parameterResponse.data.response;
-      // const parameterResponse = await axios.get(import.meta.env.VITE_API_URL + '/api/parameter');
-      // const parameters = parameterResponse.data.response;
-      // const currentValuesObj = parameters.find((parameter) => parameter._id === '676d6f02d6ae0a6ca083bee6');
-      // setParameterData(currentValuesObj);
-      // Log the parameter data to the console to inspect it
-      
-
-      // Set the parameter data
-//       if (valuesObj) {
-//         setParameterData(valuesObj);
-//       } else {
-//         console.error('Parameter with the given ID not found.');
-//       }
-//     } catch (error) {
-//       console.error('Error fetching data:', error);
-//     }
-//   };
-
-
-
-
-//   fetchData();
-//   const interval = setInterval(fetchData, 10000); 
-
-//   return () => clearInterval(interval);
-// }, []);
-
-useEffect(() => {
-  const fetchData = async () => {
-    try {
-      // Fetch vanguard data
-      const vanguardResponse = await axios.get(import.meta.env.VITE_API_URL + '/api/vanguard');
-      const vanguards = vanguardResponse.data.response;
-      console.log('Datos de Parameters:', vanguardData);
-      const currentTemperatureObj = vanguards.find((vanguard) => vanguard._id === '6609c0e76cfbe770c4735e09');
-      setVanguardData(currentTemperatureObj);
-
-      // Fetch parameters data
-      // const parameterResponse = await axios.get(import.meta.env.VITE_API_URL + '/api/parameter');
-      // const parameters = parameterResponse.data.response;
-      // console.log('Datos de Parameters:', parameters);
-      // const currentValuesObj = parameters.find((parameter) => parameter._id === '676d6f02d6ae0a6ca083bee6');
-      // setParameters(currentValuesObj); 
-    } catch (error) {
-      console.error('Error fetching data:', error);
-    }
-  };
-
-  fetchData();
-}, []);
-
+    fetchData();
+  }, []);
 
   return (
     <div className="vanguard-container">
@@ -154,17 +136,19 @@ useEffect(() => {
               <p>Time: {formatTime(vanguardData.date)}</p>
             </div>
           </div>
+        </div>
+      )}
 
-          {/* Tarjeta de parámetros */}
-          <div className="card parameters-card">
+      {ajustesData && (
+        <div className="vanguard-content">
+          {/* Tarjeta de ajustes */}
+          <div className="card ajustes-card">
             <div className="card-body">
-              <h5 className="card-title">Parameters</h5>
-              <div>
-                <p>Min Temperature: {parameters.tempMin} °C</p>
-                <p>Max Temperature: {parameters.tempMax} °C</p>
-                <p>Min Humidity: {parameters.humidityMin} %</p>
-                <p>Max Humidity: {parameters.humidityMax} %</p>
-              </div>
+              <h5 className="card-title">Ajustes</h5>
+              <p>Min Temperature: {ajustesData.tempMin} °C</p>
+              <p>Max Temperature: {ajustesData.tempMax} °C</p>
+              <p>Min Humidity: {ajustesData.humidyMin} %</p>
+              <p>Max Humidity: {ajustesData.humidyMax} %</p>
             </div>
           </div>
         </div>
