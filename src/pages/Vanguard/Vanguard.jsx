f0ca62496ffd029ffa63643691c2a710878660f4
GuillePfarr <guillepfarr@hotmail.com>
2024-12-27 10:18:34 -0300

    New try Vanguard.jsx

--- src/pages/Vanguard/Vanguard.jsx
+++ src/pages/Vanguard/Vanguard.jsx
@@ -101,20 +101,23 @@ function Vanguard() {
 
   useEffect(() => {
     const fetchData = async () => {
-      try {
-        // Fetch vanguard data
-        const vanguardResponse = await axios.get(import.meta.env.VITE_API_URL + '/api/vanguard');
-        const vanguards = vanguardResponse.data.response;
-        const currentTemperatureObj = vanguards.find((vanguard) => vanguard._id === '6609c0e76cfbe770c4735e09');
-        setVanguardData(currentTemperatureObj);
+  try {
+    // Fetch vanguard data
+    const vanguardResponse = await axios.get(import.meta.env.VITE_API_URL + '/api/vanguard');
+    const vanguards = vanguardResponse.data.response;
+    const currentTemperatureObj = vanguards.find((vanguard) => vanguard._id === '6609c0e76cfbe770c4735e09');
+    setVanguardData(currentTemperatureObj);
 
-        // Fetch ajustes data
-        const ajustesResponse = await axios.get('https://mytinerary-2s20.onrender.com/api/ajuste/676e68bcce18f424bfd35d5a');
-        setAjustesData(ajustesResponse.data); // Guardamos los datos de ajustes
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
+    // Fetch ajustes data
+    const ajustesResponse = await axios.get(import.meta.env.VITE_API_URL + '/api/ajuste'); // Cambiar a endpoint correcto
+    const ajustes = ajustesResponse.data.response; // Suponiendo que los datos se devuelven con la misma estructura que vanguard
+    const currentAjusteObj = ajustes.find((ajuste) => ajuste._id === '676e68bcce18f424bfd35d5a'); // Asegúrate de que este ID sea correcto
+    setAjustesData(currentAjusteObj); // Guardamos los datos de ajustes
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+};
+
 
     fetchData();
   }, []);
